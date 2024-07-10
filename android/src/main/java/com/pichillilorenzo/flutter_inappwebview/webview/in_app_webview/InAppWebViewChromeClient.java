57295fe6c956adccc86fb86092a02984f784a28a
yangguoqiang <15228967773@qq.comcom>
2024-07-10 15:20:16 +0800

    测试

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/webview/in_app_webview/InAppWebViewChromeClient.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/webview/in_app_webview/InAppWebViewChromeClient.java
@@ -855,6 +855,7 @@ public class InAppWebViewChromeClient extends WebChromeClient implements PluginR
 
   @Override
   public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+    Log.i("onActivityResult","================");
     if (filePathCallback == null && filePathCallbackLegacy == null) {
       return true;
     }
